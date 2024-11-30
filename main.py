44d3ddfd15e71a0337ea21d13be325b35dced373
Max <max74327@yandex.ru>
2024-11-30 19:33:44 +0400

    1

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6 import QPoint
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt6 import uic
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        uic.loadUi('UI.ui', self)
+        self.setWindowTitle('Рисование')
+        self.btn.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        R = randint(20, 100)
+        x = randint(0, 500)
+        y = randint(0, 500)
+        self.qp.serBrush(QColor(255, 255, 0))
+        self.qp.drawEllipse(QPoint(x, y), R / 2, R / 2)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
